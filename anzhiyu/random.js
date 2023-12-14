cdc3ac83e0c7f1b8ddfd1f8c75853daa7889b347
jitcor <louis4china@gmail.com>
2023-12-14 18:35:58 +0000

    删除无用页面

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/12/14/Github博客搭建笔记/","2023/12/14/hello-world/","2023/12/14/help/"];function toRandomPost(){
+var posts=["2023/12/14/Github博客搭建笔记/","2023/12/14/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
