2e66830e852be124d7fa60078d55669835340d82
jitcor <louis4china@gmail.com>
2023-12-15 19:07:35 +0000

    feat(blog): 更新博客搭建笔记中的图片路径
    博客搭建笔记中的图片路径发生了变化，更新了图片的路径引用。

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/12/14/Github博客搭建笔记/","2023/12/15/hello-world/","2023/12/14/help/"];function toRandomPost(){
+var posts=["2023/12/14/Github博客搭建笔记/","2023/12/15/hello-world/","2023/12/14/help/","2023/12/15/阅读书源制作笔记/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
